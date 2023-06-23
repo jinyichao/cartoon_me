8299d94d0ce022e7245e70a5d4403bb9aa84db63
jinyichao <jinyichao@gmail.com>
2023-06-23 17:40:20 +0800

    adjust dtype for cuda and mps

--- img2img.py
+++ img2img.py
@@ -2,6 +2,7 @@ from diffusers import StableDiffusionImg2ImgPipeline
 import torch
 
 device = "cuda" if torch.cuda.is_available() else "mps"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 sd_model_path = "danbrown/RevAnimated-v1-2-2"
 lora_model_path = "./lora/blindbox_v1_mix.safetensors"
 
@@ -13,7 +14,7 @@ class ImageConvert:
     def __init__(self):
         self.pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
             sd_model_path,
-            torch_dtype=torch.float16,
+            torch_dtype=torch_dtype,
             safety_checker=None,
             requires_safety_checker=False,
         )
