21cb346b8a9d2f43d7f9577f1b9746056e9a0649
jinyichao <jinyichao@gmail.com>
2023-06-27 11:17:41 +0800

    add controlnet support for QR code embedding

--- img2img.py
+++ img2img.py
@@ -1,38 +1,76 @@
-from diffusers import StableDiffusionImg2ImgPipeline
 import torch
+import io
+import pyqrcode
+from PIL import Image
+from diffusers import StableDiffusionControlNetImg2ImgPipeline, StableDiffusionImg2ImgPipeline, \
+    ControlNetModel, UniPCMultistepScheduler, EulerDiscreteScheduler
+
 
 device = "cuda" if torch.cuda.is_available() else "mps"
 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 sd_model_path = "danbrown/RevAnimated-v1-2-2"
-# lora_model_path = "./lora/blindbox_v1_mix.safetensors"
+controlnet_path = "DionTimmer/controlnet_qrcode-control_v1p_sd15"
 
 negative_prompt = "(worst quality, low quality:1.4), EasyNegative, nsfw, naked, watermark, angry, sad"
 
 
 class ImageConvert:
     def __init__(self):
-        self.pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
+        self.controlnet = ControlNetModel.from_pretrained(controlnet_path, torch_dtype=torch_dtype)
+
+        self.img2img_pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
             sd_model_path,
             torch_dtype=torch_dtype,
             safety_checker=None,
             requires_safety_checker=False,
         )
+        self.pipe = StableDiffusionControlNetImg2ImgPipeline.from_pretrained(
+            sd_model_path,
+            controlnet=self.controlnet,
+            torch_dtype=torch_dtype,
+            safety_checker=None,
+            requires_safety_checker=False,
+        )
+        self.img2img_pipe = self.img2img_pipe.to(device)
         self.pipe = self.pipe.to(device)
-        # self.pipe.load_lora_weights(lora_model_path)
         if device == "cuda":
+            self.img2img_pipe.enable_xformers_memory_efficient_attention()
+            self.img2img_pipe.enable_attention_slicing()
+            self.img2img_pipe.enable_sequential_cpu_offload()
             self.pipe.enable_xformers_memory_efficient_attention()
             self.pipe.enable_attention_slicing()
             self.pipe.enable_sequential_cpu_offload()
+            self.pipe.scheduler = UniPCMultistepScheduler.from_config(self.pipe.scheduler.config)
+        else:
+            self.pipe.scheduler = EulerDiscreteScheduler.from_config(self.pipe.scheduler.config)
 
-    def generate_image(self, image, prompt, strength=0.6):
+    def generate_image(self, image, prompt, qr_data=None, strength=0.6):
         init_image = image.convert("RGB")
-        init_image = init_image.resize((512, 512))
+        if qr_data:
+            w, h = 768, 768
+            qrobject = pyqrcode.create(qr_data, error="H")
+            buffer = io.BytesIO()
+            qrobject.png(buffer, scale=20)
+            qr_image = Image.open(buffer).resize((w, h))
 
-        images = self.pipe(
-            prompt=prompt,
-            negative_prompt=negative_prompt,
-            image=init_image,
-            strength=strength,
-            guidance_scale=7.5,
-        ).images
+            images = self.pipe(
+                prompt=prompt,
+                negative_prompt=negative_prompt,
+                image=init_image.resize((w, h)),
+                control_image=qr_image,
+                strength=strength,
+                guidance_scale=5.5,
+                controlnet_conditioning_scale=2.2,
+                width=w,
+                height=h,
+            ).images
+        else:
+            w, h = 512, 512
+            images = self.img2img_pipe(
+                prompt=prompt,
+                negative_prompt=negative_prompt,
+                image=init_image.resize((w, h)),
+                strength=strength,
+                guidance_scale=7.5,
+            ).images
         return images[0]
