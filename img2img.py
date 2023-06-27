409a0631b1d97a195311ebb1d22ac32843630008
jinyichao <jinyichao@gmail.com>
2023-06-27 11:41:05 +0800

    reuse StableDiffusionControlNetImg2ImgPipeline for both use cases

--- img2img.py
+++ img2img.py
@@ -2,8 +2,7 @@ import torch
 import io
 import pyqrcode
 from PIL import Image
-from diffusers import StableDiffusionControlNetImg2ImgPipeline, StableDiffusionImg2ImgPipeline, \
-    ControlNetModel, UniPCMultistepScheduler, EulerDiscreteScheduler
+from diffusers import StableDiffusionControlNetImg2ImgPipeline, ControlNetModel, UniPCMultistepScheduler
 
 
 device = "cuda" if torch.cuda.is_available() else "mps"
@@ -18,12 +17,6 @@ class ImageConvert:
     def __init__(self):
         self.controlnet = ControlNetModel.from_pretrained(controlnet_path, torch_dtype=torch_dtype)
 
-        self.img2img_pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
-            sd_model_path,
-            torch_dtype=torch_dtype,
-            safety_checker=None,
-            requires_safety_checker=False,
-        )
         self.pipe = StableDiffusionControlNetImg2ImgPipeline.from_pretrained(
             sd_model_path,
             controlnet=self.controlnet,
@@ -31,18 +24,12 @@ class ImageConvert:
             safety_checker=None,
             requires_safety_checker=False,
         )
-        self.img2img_pipe = self.img2img_pipe.to(device)
         self.pipe = self.pipe.to(device)
         if device == "cuda":
-            self.img2img_pipe.enable_xformers_memory_efficient_attention()
-            self.img2img_pipe.enable_attention_slicing()
-            self.img2img_pipe.enable_sequential_cpu_offload()
             self.pipe.enable_xformers_memory_efficient_attention()
             self.pipe.enable_attention_slicing()
             self.pipe.enable_sequential_cpu_offload()
             self.pipe.scheduler = UniPCMultistepScheduler.from_config(self.pipe.scheduler.config)
-        else:
-            self.pipe.scheduler = EulerDiscreteScheduler.from_config(self.pipe.scheduler.config)
 
     def generate_image(self, image, prompt, qr_data=None, strength=0.6):
         init_image = image.convert("RGB")
@@ -51,26 +38,21 @@ class ImageConvert:
             qrobject = pyqrcode.create(qr_data, error="H")
             buffer = io.BytesIO()
             qrobject.png(buffer, scale=20)
-            qr_image = Image.open(buffer).resize((w, h))
-
-            images = self.pipe(
-                prompt=prompt,
-                negative_prompt=negative_prompt,
-                image=init_image.resize((w, h)),
-                control_image=qr_image,
-                strength=strength,
-                guidance_scale=5.5,
-                controlnet_conditioning_scale=2.2,
-                width=w,
-                height=h,
-            ).images
+            control_image = Image.open(buffer).resize((w, h))
+            conditioning_scale = 2.6
+            guidance_scale = 5.5
         else:
             w, h = 512, 512
-            images = self.img2img_pipe(
-                prompt=prompt,
-                negative_prompt=negative_prompt,
-                image=init_image.resize((w, h)),
-                strength=strength,
-                guidance_scale=7.5,
-            ).images
+            control_image = Image.new("RGB", (w, h))
+            conditioning_scale = 0.0
+            guidance_scale = 7.5
+        images = self.pipe(
+            prompt=prompt,
+            negative_prompt=negative_prompt,
+            image=init_image.resize((w, h)),
+            control_image=control_image,
+            strength=strength,
+            guidance_scale=guidance_scale,
+            controlnet_conditioning_scale=conditioning_scale,
+        ).images
         return images[0]
